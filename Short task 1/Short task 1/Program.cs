73ed3c524210d9bde09dbfacf3eff097661e7200
Anastasya Lukovskaya <fafnirra@gmail.com>
2020-08-31 14:09:01 +0300

    c# short task 1

---
+++ Short task 1/Short task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Short_task_1
+{
+    class Program
+    {
+        public static int FindLargestNum(int[] arr)
+        {
+            return arr.Max();
+        }
+    }
+}
